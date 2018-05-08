cf0ca1fc4a63dc82685704ef48546a3343579e46
Jen <jennifer.delavega@outlook.com>
2018-05-07 23:39:45 -0700

    master

---
+++ StackLayout/StackLayout/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace StackLayout
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+	    private void Login(object sender, EventArgs e)
+	    {
+	        DisplayAlert("Welcome", "Login success!", "OK");
+	        Navigation.PushAsync(new Excer2());
+	    }
+    }
+}
